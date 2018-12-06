d3975804454881b9925357d35f9d7b94463b58c9
Hanying Dong <hanying@maggylondon.com>
2018-12-06 09:47:09 -0500

    add mauually export shipments

---
+++ ml-324-manually-export-shipments.sql
@@ -0,0 +1,9 @@
+select so.increment_id as order_number, group_concat(sst.track_number SEPARATOR ',') as tracking_numbers, max(sst.created_at) as last_shipment_date
+from sales_shipment_track sst
+join sales_order so
+on so.entity_id = sst.order_id
+join ml_amazon_order mao
+on mao.order_id = so.entity_id 
+where so.status = 'complete'
+and so.increment_id not in (/*'1234567890','2234567890'*/)
+group by so.increment_id ;
